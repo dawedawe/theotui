423142019df54990854b4801490054f9b3e1a4b9
dawe <dawedawe@posteo.de>
2026-02-15 20:57:55 +0100

    feat: first stab at eval for set theory

--- theoinf/src/set_theory.rs
+++ theoinf/src/set_theory.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+use std::ops::Deref;
+
 use winnow::ModalResult;
 use winnow::Parser;
 use winnow::ascii::multispace0;
@@ -90,7 +93,54 @@ fn identifier<'i>(i: &mut &'i str) -> ModalResult<&'i str> {
 }
 
 fn comma_list<'i>(i: &mut &'i str) -> ModalResult<Vec<&'i str>> {
-    separated(0.., identifier, ",").parse_next(i)
+    let ident_with_space = delimited(multispace0, identifier, multispace0);
+    separated(0.., ident_with_space, ",").parse_next(i)
+}
+
+pub fn eval(expr: &Expr) -> Expr {
+    match expr {
+        Expr::Var(_) => expr.clone(),
+        Expr::Paren(a) => eval(a),
+        Expr::SetLiteral(_items) => expr.clone(),
+        Expr::SetDecl(_, _expr) => todo!(),
+        Expr::Not(_expr) => todo!(),
+        Expr::Intersection(expr1, expr2) => {
+            let expr1 = eval(expr1);
+            let expr2 = eval(expr2);
+            match (expr1, expr2) {
+                (Expr::SetLiteral(items1), Expr::SetLiteral(items2)) => {
+                    let set1: HashSet<String> = items1.deref().iter().cloned().collect();
+                    let set2: HashSet<String> = items2.deref().iter().cloned().collect();
+                    let mut inter: Vec<String> = set1.intersection(&set2).cloned().collect();
+                    inter.sort();
+                    Expr::SetLiteral(inter)
+                }
+                _ => todo!(),
+            }
+        }
+        Expr::Union(expr1, expr2) => {
+            let expr1 = eval(expr1);
+            let expr2 = eval(expr2);
+            match (expr1, expr2) {
+                (Expr::SetLiteral(items1), Expr::SetLiteral(items2)) => {
+                    let set1: HashSet<String> = items1.deref().iter().cloned().collect();
+                    let set2: HashSet<String> = items2.deref().iter().cloned().collect();
+                    let mut union: Vec<String> = set1.union(&set2).cloned().collect();
+                    union.sort();
+                    Expr::SetLiteral(union)
+                }
+                _ => todo!(),
+            }
+        }
+    }
+}
+
+pub fn run(formula: &str) -> Result<Expr, String> {
+    let input = formula.to_string();
+    match pratt_parser(&mut input.as_str()) {
+        Ok(expr) => Ok(eval(&expr)),
+        Err(e) => Result::Err(e.to_string()),
+    }
 }
 
 #[cfg(test)]
@@ -149,11 +199,71 @@ mod tests {
     }
 
     #[test]
+    fn parsing_a_union_works() {
+        let mut input = "{ a , b,c} u {d,e}";
+        let expr = pratt_parser(&mut input);
+        assert!(expr.is_ok());
+        let s0 = Expr::SetLiteral(vec!["a".into(), "b".into(), "c".into()]);
+        let s1 = Expr::SetLiteral(vec!["d".into(), "e".into()]);
+        assert_eq!(Expr::Union(Box::new(s0), Box::new(s1)), expr.unwrap());
+        assert_eq!("", input);
+    }
+
+    #[test]
+    fn parsing_an_intersection_works() {
+        let mut input = "{ a } n {}";
+        let expr = pratt_parser(&mut input);
+        assert!(expr.is_ok());
+        let s0 = Expr::SetLiteral(vec!["a".into()]);
+        let s1 = Expr::SetLiteral(vec![]);
+        assert_eq!(
+            Expr::Intersection(Box::new(s0), Box::new(s1)),
+            expr.unwrap()
+        );
+        assert_eq!("", input);
+    }
+
+    #[test]
     fn parsing_a_comma_list_works() {
-        let mut input = "a,b,c";
+        let mut input = "a,b, c";
         let expr = comma_list(&mut input);
         assert!(expr.is_ok());
         assert_eq!(vec!["a", "b", "c"], expr.unwrap());
         assert_eq!("", input);
+    }
+
+    #[test]
+    fn eval_of_an_intersection_works() {
+        let r = run("{ a,c } n {a,b,c}");
+        assert!(r.is_ok());
+
+        assert_eq!(Expr::SetLiteral(vec!["a".into(), "c".into()]), r.unwrap());
+    }
+
+    #[test]
+    fn eval_of_an_intersection_with_an_empty_set_works() {
+        let r = run("{ a,c } n {}");
+        assert!(r.is_ok());
+
+        assert_eq!(Expr::SetLiteral(vec![]), r.unwrap());
+    }
+
+    #[test]
+    fn eval_of_an_union_works() {
+        let r = run("{ a,c } u {b,c}");
+        assert!(r.is_ok());
+
+        assert_eq!(
+            Expr::SetLiteral(vec!["a".into(), "b".into(), "c".into()]),
+            r.unwrap()
+        );
+    }
+
+    #[test]
+    fn eval_of_an_union_with_an_empty_set_works() {
+        let r = run("({} u ({b,c}))");
+        assert!(r.is_ok());
+
+        assert_eq!(Expr::SetLiteral(vec!["b".into(), "c".into()]), r.unwrap());
     }
 }
